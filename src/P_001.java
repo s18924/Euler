d1e0efc518e8fffab4f69d7ffaaf84be11879170
s18924 <s18924@pjwstk.edu.pl>
2020-08-21 22:42:01 +0200

    Problem 1 - Hackerrank solved

--- src/P_001.java
+++ src/P_001.java
@@ -1,10 +1,33 @@
+import java.math.BigDecimal;
+
 public class P_001 {
     public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 0; i < 1000; i++) {
-            if (i % 3 == 0 || i % 5 == 0)
-                sum += i;
-        }
-        System.out.println(sum);
+        double time = System.currentTimeMillis();
+
+        int n = 9;
+
+
+        BigDecimal no3div = new BigDecimal(Math.floorDiv(n,3));
+        BigDecimal mediumValueOf3 = new BigDecimal((n/2.0)+ (3/2.0- (n%3)/2.0));
+        no3div = no3div.multiply(mediumValueOf3);
+        System.out.println(">" + no3div);
+
+        BigDecimal no5div = new BigDecimal(Math.floorDiv(n,5));
+        BigDecimal mediumValueOf5 = new BigDecimal((n/2.0)+ (2.5 - (n%5)/2.0));
+        no5div = no5div.multiply(mediumValueOf5);
+        System.out.println(">" + no5div);
+
+        BigDecimal no15div = new BigDecimal(Math.floorDiv(n,15));
+        BigDecimal mediumValueOf15 = new BigDecimal((n/2.0)+ (7.5 - (n%15)/2.0));
+        no15div = no15div.multiply(mediumValueOf15);
+        System.out.println(">" + no15div);
+
+
+        System.out.println(
+                no3div.add(no5div).add(no15div.multiply(BigDecimal.valueOf(-1))).toBigInteger()
+        );
+
+
+       System.out.println(System.currentTimeMillis() - time);
     }
 }
---
+++ src/P_034.java
@@ -0,0 +1,7 @@
+public class P_034 {
+    public static void main(String[] args) {
+    /*    for (int i = 10; i < ; i++) {
+
+        }*/
+    }
+}
